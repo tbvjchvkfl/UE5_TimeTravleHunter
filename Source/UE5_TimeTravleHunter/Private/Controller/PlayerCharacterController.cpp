281186bf1cbf59a3625a603199334cfaef49b06e
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-08-24 20:39:50 +0900

    controller 코드 수정

--- Source/UE5_TimeTravleHunter/Private/Controller/PlayerCharacterController.cpp
+++ Source/UE5_TimeTravleHunter/Private/Controller/PlayerCharacterController.cpp
@@ -1,5 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+// GameFramework
 #include "Controller/PlayerCharacterController.h"
+#include "Character/Player/PlayerCharacter.h"
 
+// Engine
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Engine/LocalPlayer.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+#include "Kismet/GameplayStatics.h"
+
+APlayerCharacterController::APlayerCharacterController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PlayerCharacter = Cast<APlayerCharacter>(GetPawn());
+}
+
+void APlayerCharacterController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APlayerCharacterController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (PlayerCharacter == nullptr)
+	{
+		PlayerCharacter = Cast<APlayerCharacter>(GetPawn());
+	}
+}
