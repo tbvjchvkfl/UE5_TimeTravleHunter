cc472c2641abbbc7a09960862e812ccec287da5d
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-09-10 22:55:01 +0900

    플레이어 캐릭터 코드 추가

---
+++ Source/UE5_TimeTravleHunter/Private/Player/PlayerCharacter.cpp
@@ -0,0 +1,270 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+// GameFramework
+#include "Character/Player/PlayerCharacter.h"
+#include "Controller/PlayerCharacterController.h"
+#include "Interface/InteractionInterface.h"
+#include "Component/InventoryComponent.h"
+#include "Object/PickUpItem.h"
+
+// Engine
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Engine/LocalPlayer.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+
+APlayerCharacter::APlayerCharacter() : 
+	MaxHealth(100.0f), 
+	CurrentHealth(MaxHealth),  
+	MaxStamina(80.0f),
+	CurrentStamina(MaxStamina), 
+	LookingRotationValue(1.0f)
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 150.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	MainCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("MainCamera"));
+	MainCamera->SetupAttachment(CameraBoom);
+	MainCamera->bUsePawnControlRotation = false;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+	GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+	GetCharacterMovement()->JumpZVelocity = 500.0f;
+	GetCharacterMovement()->AirControl = 1.0f;
+
+	ItemInventory = CreateDefaultSubobject<UInventoryComponent>(TEXT("InventoryComponent"));
+
+	for (int i = 0; i < 2; i++)
+	{
+		TestInventory.Push(i);
+	}
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (GetCharacterMovement()->IsFalling())
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Green, FString::Printf(TEXT("Falling")), true);
+	}
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (APlayerCharacterController *PlayerController = Cast<APlayerCharacterController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem *const SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			SubSystem->AddMappingContext(DefaultContext, 0);
+		}
+	}
+
+	if (UEnhancedInputComponent *const EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(Moving, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+
+		EnhancedInputComponent->BindAction(Sprinting, ETriggerEvent::Triggered, this, &APlayerCharacter::Sprint);
+
+		EnhancedInputComponent->BindAction(Crouching, ETriggerEvent::Triggered, this, &APlayerCharacter::Crouch);
+
+		EnhancedInputComponent->BindAction(Looking, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+
+		EnhancedInputComponent->BindAction(Jumping, ETriggerEvent::Started, this, &APlayerCharacter::Jump);
+		EnhancedInputComponent->BindAction(Jumping, ETriggerEvent::Completed, this, &APlayerCharacter::StopJumping);
+
+		EnhancedInputComponent->BindAction(MoveSwitching, ETriggerEvent::Triggered, this, &APlayerCharacter::WalktoJog);
+
+		EnhancedInputComponent->BindAction(Interacting, ETriggerEvent::Triggered, this, &APlayerCharacter::Interaction);
+
+		EnhancedInputComponent->BindAction(Aimming, ETriggerEvent::Started, this, &APlayerCharacter::StartAimming);
+		EnhancedInputComponent->BindAction(Aimming, ETriggerEvent::Completed, this, &APlayerCharacter::StopAimming);
+
+		EnhancedInputComponent->BindAction(SwitchingMainWeapon, ETriggerEvent::Triggered, this, &APlayerCharacter::SwitchingWeaponMain);
+		EnhancedInputComponent->BindAction(SwitchingSubWeapon, ETriggerEvent::Triggered, this, &APlayerCharacter::SwitchingWeaponSub);
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue &Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+	AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void APlayerCharacter::Look(const FInputActionValue &Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+	AddControllerYawInput(LookAxisVector.X * LookingRotationValue * GetWorld()->GetDeltaSeconds());
+	AddControllerPitchInput(LookAxisVector.Y * LookingRotationValue * GetWorld()->GetDeltaSeconds());
+}
+
+void APlayerCharacter::Sprint(const FInputActionValue &Value)
+{
+	if (!bIsSprint)
+	{
+		bIsSprint = true;
+		GetCharacterMovement()->MaxWalkSpeed = 800.0f;
+	}
+	else
+	{
+		bIsSprint = false;
+		GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+	}
+}
+
+void APlayerCharacter::Crouch(const FInputActionValue &Value)
+{
+	if (!bIsCrouch)
+	{
+		bIsCrouch = true;
+		GetCharacterMovement()->MaxWalkSpeed = 150.0f;
+	}
+	else
+	{
+		bIsCrouch = false;
+		if (bIsWalk)
+		{
+			GetCharacterMovement()->MaxWalkSpeed = 200.0f;
+		}
+		else
+		{
+			GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+		}
+	}
+}
+
+void APlayerCharacter::WalktoJog(const FInputActionValue &Value)
+{
+	if (!bIsWalk)
+	{
+		bIsWalk = true;
+		GetCharacterMovement()->MaxWalkSpeed = 200.0f;
+	}
+	else
+	{
+		bIsWalk = false;
+		GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+	}
+}
+
+void APlayerCharacter::StartAimming()
+{
+	if (!bIsAimming)
+	{
+		bIsAimming = true;
+		bUseControllerRotationYaw = true;
+		GetCharacterMovement()->bOrientRotationToMovement = false;
+		GetCharacterMovement()->MaxWalkSpeed = 200.0f;
+		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Cyan, FString::Printf(TEXT("Aimming")), true);
+	}
+}
+
+void APlayerCharacter::StopAimming()
+{
+	if (bIsAimming)
+	{
+		bIsAimming = false;
+		bUseControllerRotationYaw = false;
+		GetCharacterMovement()->bOrientRotationToMovement = true;
+		if (!bIsWalk)
+		{
+			GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+		}
+	}
+}
+
+void APlayerCharacter::SwitchingWeaponMain()
+{
+	TestEquip = TestInventory[0];
+	if (TestEquip == 0)
+	{
+		bIsPistol = true;
+		bIsRifle = false;
+		bIsShotgun = false;
+	}
+	if (TestEquip == 1)
+	{
+		bIsPistol = false;
+		bIsRifle = true;
+		bIsShotgun = false;
+	}
+	if (bIsShotgun)
+	{
+
+	}
+}
+
+void APlayerCharacter::SwitchingWeaponSub()
+{
+	if (bIsPistol)
+	{
+		bIsPistol = false;
+	}
+	if (bIsRifle)
+	{
+
+	}
+	if (bIsShotgun)
+	{
+
+	}
+}
+
+void APlayerCharacter::SprintCameraMoving()
+{
+	if (bIsSprint)
+	{
+
+	}
+}
+
+void APlayerCharacter::Interaction()
+{
+	FVector StartPos{ GetPawnViewLocation() + FVector(0.0f, 0.0f, 10.0f) };
+	FVector EndPos{ StartPos + (GetViewRotation().Vector() * 1000.0f) };
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(this);
+	FHitResult HitTrace;
+	DrawDebugLine(GetWorld(), StartPos, EndPos, FColor::Red, false, 3.0f);
+	if (GetWorld()->LineTraceSingleByChannel(HitTrace, StartPos, EndPos, ECollisionChannel::ECC_GameTraceChannel12, QueryParams))
+	{
+		InteractionInterface = HitTrace.GetActor();
+		if (auto PickUpItem = Cast<APickUpItem>(HitTrace.GetActor()))
+		{
+			InteractionInterface->PickUpInteraction(this);
+		}
+	}
+}
+
+void APlayerCharacter::TestFunction()
+{
+
+}
