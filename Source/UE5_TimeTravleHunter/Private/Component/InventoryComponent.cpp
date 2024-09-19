0b4eb3238151958c848d6b613a2f1ce5da63236d
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-09-19 22:23:58 +0900

    InventoryComponent 코드 추가

--- Source/UE5_TimeTravleHunter/Private/Component/InventoryComponent.cpp
+++ Source/UE5_TimeTravleHunter/Private/Component/InventoryComponent.cpp
@@ -9,25 +9,127 @@ UInventoryComponent::UInventoryComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 	CoinInventory = 0;
+	Columns = 15;
+	Rows = 6;
 }
 
-void UInventoryComponent::AddItemInventory(class APickUpItem *Item)
+void UInventoryComponent::AddInventory(class APickUpItem *Item)
 {
 	// Add to InventoryArray
-	if (Item->GetItemType() == EItemType::Coin)
+	switch (Item->GetItemType())
 	{
-		CoinInventory += Item->GetItemAmount();
-	}
-	else
-	{
-		ItemInventory.Add(Item);
+		case EItemType::Coin:
+		{
+			CoinInventory += Item->GetItemAmount();
+		}
+		break;
+		case EItemType::Weapon:
+		{
+			AddItem(Item);
+		}
+		break;
+		case EItemType::Consumable:
+		{
+			AddItem(Item);
+		}
+		break;
+		default:
+		{
+			return;
+		}
+		break;
 	}
 	OnInventoryUpdate.Broadcast();
+}
+
+TMap<class APickUpItem *, FVector2D> UInventoryComponent::GetAllItems()
+{
+	TMap<APickUpItem*, FVector2D> Items;
+
+	for (int i = 0; i < ItemInventory.Num(); i++)
+	{
+		if (ItemInventory[i] != nullptr && !Items.Contains(ItemInventory[i]))
+		{
+			Items.Add(ItemInventory[i], FVector2D(i % Columns, i / Columns));
+		}
+	}
+	return Items;
+}
+
+
+
+void UInventoryComponent::AddItem(APickUpItem *Items)
+{
+	if (Items)
+	{
+		for (int32 i = 0; i < ItemInventory.Num(); i++)
+		{
+			if (bIsRoomAvailable(Items, i))
+			{
+				AddingItem(Items, i);
+			}
+		}
+		for (int32 i = 0; i < ItemInventory.Num(); i++)
+		{
+			Items->ToggleRotated();
+			if (bIsRoomAvailable(Items, i))
+			{
+				AddingItem(Items, i);
+			}
+			Items->ToggleRotated();
+		}
+	}
+}
+
+void UInventoryComponent::AddingItem(class APickUpItem *Items, int32 TopLeftIndex)
+{
+	int32 tileX = TopLeftIndex % Columns;
+	int32 tileY = TopLeftIndex / Columns;
+	for (int32 n = tileX; n < tileX + Items->GetDimensions().X; n++)
+	{
+		for (int32 m = tileY; m < tileY + Items->GetDimensions().Y; m++)
+		{
+			int32 index = n + m * Columns;
+			ItemInventory[index] = Items;
+		}
+	}
+	GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, FString::Printf(TEXT("Add Success")), true);
+	bIsDirty = true;
+}
+
+bool UInventoryComponent::bIsRoomAvailable(APickUpItem *Items, int32 TopLeftIndex) const
+{
+	int32 tileX = TopLeftIndex % Columns;
+	int32 tileY = TopLeftIndex / Columns;
+	for (int32 n = tileX; n < tileX + Items->GetDimensions().X; n++)
+	{
+		for (int32 m = tileY; m < tileY + Items->GetDimensions().Y; m++)
+		{
+			if (n >= 0 && m >= 0 && n < Columns && m < Rows)
+			{
+				int32 index = n + m * Columns;
+				if (!ItemInventory.IsValidIndex(index))
+				{
+					return false;
+				}
+				if (ItemInventory[index] != nullptr)
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 void UInventoryComponent::BeginPlay()
 {
 	Super::BeginPlay();
+	ItemInventory.Init(nullptr, Rows * Columns);
 }
 
 void UInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
