ea3e7e8883bbd838b939877e6d32f02b9fb9365b
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-10-12 22:03:54 +0900

    InventoryComponent 로직 수정 (TArray->TMap으로 변경)

--- Source/UE5_TimeTravleHunter/Private/Component/InventoryComponent.cpp
+++ Source/UE5_TimeTravleHunter/Private/Component/InventoryComponent.cpp
@@ -9,8 +9,34 @@ UInventoryComponent::UInventoryComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 	CoinInventory = 0;
-	Columns = 15;
-	Rows = 6;
+	InventorySize = FIntPoint(10, 4);
+}
+
+
+void UInventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	InitializeInventory();
+}
+
+void UInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UInventoryComponent::InitializeInventory()
+{
+	ItemInventory.Empty();
+	if (ItemInventory.IsEmpty())
+	{
+		for (int32 n = 0; n < InventorySize.Y; n++)
+		{
+			for (int32 m = 0; m < InventorySize.X; m++)
+			{
+				ItemInventory.Add(FVector2D(m, n), nullptr);
+			}
+		}
+	}
 }
 
 void UInventoryComponent::AddInventory(class APickUpItem *Item)
@@ -20,7 +46,7 @@ void UInventoryComponent::AddInventory(class APickUpItem *Item)
 	{
 		case EItemType::Coin:
 		{
-			CoinInventory += Item->GetItemAmount();
+			CoinInventory += Item->GetCurrentQuantity();
 		}
 		break;
 		case EItemType::Weapon:
@@ -42,98 +68,110 @@ void UInventoryComponent::AddInventory(class APickUpItem *Item)
 	OnInventoryUpdate.Broadcast();
 }
 
-TMap<class APickUpItem *, FVector2D> UInventoryComponent::GetAllItems()
-{
-	TMap<APickUpItem*, FVector2D> Items;
-
-	for (int i = 0; i < ItemInventory.Num(); i++)
-	{
-		if (ItemInventory[i] != nullptr && !Items.Contains(ItemInventory[i]))
-		{
-			Items.Add(ItemInventory[i], FVector2D(i % Columns, i / Columns));
-		}
-	}
-	return Items;
-}
-
-
+//TMap<class APickUpItem *, FVector2D> UInventoryComponent::GetAllItems()
+//{
+//	TMap<APickUpItem*, FVector2D> Items;
+//
+//	for (int i = 0; i < ItemInventory.Num(); i++)
+//	{
+//		if (ItemInventory[i] != nullptr && !Items.Contains(ItemInventory[i]))
+//		{
+//			Items.Add(ItemInventory[i], FVector2D(i % Columns, i / Columns));
+//		}
+//	}
+//	return Items;
+//}
 
 void UInventoryComponent::AddItem(APickUpItem *Items)
 {
 	if (Items)
 	{
-		for (int32 i = 0; i < ItemInventory.Num(); i++)
+		if (bIsRoomAvailable(Items->GetShape(0.0f)))
 		{
-			if (bIsRoomAvailable(Items, i))
-			{
-				AddingItem(Items, i);
-			}
-		}
-		for (int32 i = 0; i < ItemInventory.Num(); i++)
-		{
-			Items->ToggleRotated();
-			if (bIsRoomAvailable(Items, i))
-			{
-				AddingItem(Items, i);
-			}
-			Items->ToggleRotated();
+			
 		}
 	}
+	/*if (Items)
+	{
+		for (int32 i = 0; i < ItemInventory.Num(); i++)
+		{
+			if (bIsRoomAvailable(Items, i))
+			{
+				AddingItem(Items, i);
+			}
+		}
+	}*/
+}
+
+//bool UInventoryComponent::CheckRotationFits(AMultiSlotItem *ItemReference, float Rotation)
+//{
+//	if (WhereShapeFits(ItemReference->GetShape(Rotation)).Key == true)
+//	{
+//		AddToInventory(WhereShapeFits(ItemReference->GetShape(Rotation)).Value, ItemReference, true);
+//		return true;
+//	}
+//	return false;
+//
+//}
+
+bool UInventoryComponent::bIsRoomAvailable(TArray<FVector2D> Shape) const
+{
+	bool bIsFaild = false;
+	TArray<FVector2D> AllKeys;
+	ItemInventory.GetKeys(AllKeys);
+	for (int32 i = 0; i < AllKeys.Num(); i++)
+	{
+		if (ItemInventory.Contains(AllKeys[i]))
+		{
+			if (ItemInventory[AllKeys[i]] == nullptr)
+			{
+				for (const auto &ItemShapeElem : Shape)
+				{
+					if (ItemInventory.Contains(FVector2D(AllKeys[i].X + ItemShapeElem.X, AllKeys[i].Y + ItemShapeElem.Y)))
+					{
+						bIsFaild = true;
+						break;
+					}
+					if (ItemInventory[FVector2D(AllKeys[i].X + ItemShapeElem.X, AllKeys[i].Y + ItemShapeElem.Y)] != nullptr)
+					{
+						bIsFaild = true;
+						break;
+					}
+				}
+				if (bIsFaild)
+				{
+					bIsFaild = false;
+				}
+				else
+				{
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+bool UInventoryComponent::bIsItemStackable(APickUpItem *Item) const
+{
+	TArray<FVector2D> AllKeys;
+	ItemInventory.GetKeys(AllKeys);
+	for (int32 i = 0; i < AllKeys.Num(); i++)
+	{
+		if (ItemInventory[AllKeys[i]]->GetItemName() == Item->GetItemName())
+		{
+			if (ItemInventory[AllKeys[i]]->GetMaxQuantity() > Item->GetCurrentQuantity())
+			{
+				return true;
+			}
+		}
+	}
+	return false;
 }
 
 void UInventoryComponent::AddingItem(class APickUpItem *Items, int32 TopLeftIndex)
 {
-	int32 tileX = TopLeftIndex % Columns;
-	int32 tileY = TopLeftIndex / Columns;
-	for (int32 n = tileX; n < tileX + Items->GetDimensions().X; n++)
-	{
-		for (int32 m = tileY; m < tileY + Items->GetDimensions().Y; m++)
-		{
-			int32 index = n + m * Columns;
-			ItemInventory[index] = Items;
-		}
-	}
-	GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, FString::Printf(TEXT("Add Success")), true);
-	bIsDirty = true;
+
 }
 
-bool UInventoryComponent::bIsRoomAvailable(APickUpItem *Items, int32 TopLeftIndex) const
-{
-	int32 tileX = TopLeftIndex % Columns;
-	int32 tileY = TopLeftIndex / Columns;
-	for (int32 n = tileX; n < tileX + Items->GetDimensions().X; n++)
-	{
-		for (int32 m = tileY; m < tileY + Items->GetDimensions().Y; m++)
-		{
-			if (n >= 0 && m >= 0 && n < Columns && m < Rows)
-			{
-				int32 index = n + m * Columns;
-				if (!ItemInventory.IsValidIndex(index))
-				{
-					return false;
-				}
-				if (ItemInventory[index] != nullptr)
-				{
-					return false;
-				}
-			}
-			else
-			{
-				return false;
-			}
-		}
-	}
-	return true;
-}
-
-void UInventoryComponent::BeginPlay()
-{
-	Super::BeginPlay();
-	ItemInventory.Init(nullptr, Rows * Columns);
-}
-
-void UInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-}
 
