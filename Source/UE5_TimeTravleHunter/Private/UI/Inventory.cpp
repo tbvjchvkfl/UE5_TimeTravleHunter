bb288524d3b6154a6cc4f85b23803e5b0694a1ad
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-10-14 21:52:40 +0900

    새로운 위젯 클래스 추가 & 인벤토리 위젯 코드 추가

--- Source/UE5_TimeTravleHunter/Private/UI/Inventory.cpp
+++ Source/UE5_TimeTravleHunter/Private/UI/Inventory.cpp
@@ -3,11 +3,13 @@
 
 // GameFramework
 #include "UI/Inventory.h"
-#include "UI/InventoryGird.h"
+#include "UI/InventoryGrid.h"
 #include "Character/Player/PlayerCharacter.h"
+#include "Object/PickUpItem.h"
 #include "Component/InventoryComponent.h"
 
 // Engine
+#include "Blueprint/DragDropOperation.h"
 
 void UInventory::NativeConstruct()
 {
@@ -15,21 +17,16 @@ void UInventory::NativeConstruct()
 	Player = Cast<APlayerCharacter>(GetOwningPlayerPawn());
 	if (Player)
 	{
-		InventoryComponent = Player->GetItemInventory();
-		TileSize = 100.0f;
-		if (InventoryComponent)
-		{
-			InventoryGrid->GridInitialize(InventoryComponent, TileSize);
-			InventoryComponent->OnInventoryUpdate.AddUObject(this, &UInventory::UpdateInventory);
-		}
+		GridWidget->InventoryComponent = Player->GetItemInventory();
+		GridWidget->InventorySize = Player->GetItemInventory()->GetItemInventorySize();
+		GridWidget->InventoryWidth = Player->GetItemInventory()->GetInventoryWidth();
+		GridWidget->WidgetInventory = Player->GetItemInventory()->GetItemInventory();
+		GridWidget->InventoryGridInitialize();
 	}
 }
 
 bool UInventory::NativeOnDrop(const FGeometry &InGeometry, const FDragDropEvent &InDragDropEvent, UDragDropOperation *InOperation)
 {
+	//auto *Item = Cast<APickUpItem>(InOperation->Payload);
 	return false;
-}
-
-void UInventory::UpdateInventory()
-{
 }
--- Source/UE5_TimeTravleHunter/Private/UI/InventoryGrid.cpp
+++ Source/UE5_TimeTravleHunter/Private/UI/InventoryGrid.cpp
@@ -4,85 +4,79 @@
 // GameFramework
 #include "UI/InventoryGrid.h"
 #include "UI/InventoryItem.h"
+#include "UI/InventorySlot.h"
 #include "Object/PickUpItem.h"
 #include "Component/InventoryComponent.h"
 #include "Character/Player/PlayerCharacter.h"
 
 // Engine
-#include "Components/Border.h"
-#include "Components/CanvasPanel.h"
-#include "Components/CanvasPanelSlot.h"
+#include "Components/GridPanel.h"
+#include "Components/GridSlot.h"
 #include "Blueprint/WidgetBlueprintLibrary.h"
 
-void UInventoryGrid::GridInitialize(class UInventoryComponent *InventoryComp, float Tilesize)
+void UInventoryGrid::InventoryGridInitialize()
 {
-	GridTileSize = Tilesize;
-	GridBorder->SetDesiredSizeScale(FVector2D(InventoryComp->GetColumns() * GridTileSize, InventoryComp->GetRows() * GridTileSize));
-	if (auto CanvasSlot = Cast<UCanvasPanelSlot>(GridBorder->Slot))
-	{
-		CanvasSlot->SetSize(FVector2D(InventoryComp->GetColumns() * GridTileSize, InventoryComp->GetRows() * GridTileSize));
-	}
-	RefreshGridInventory();
-	CreateLine(InventoryComp, GridTileSize);
-	//InventoryComp->OnInventoryUpdate.AddDynamic(this, &UInventoryGird::RefreshGridInventory);
-	InventoryComp->OnInventoryUpdate.AddUObject(this, &UInventoryGrid::RefreshGridInventory);
+	RefreshGrid();
+	InventoryComponent->OnInventoryUpdate.AddUObject(this, &UInventoryGrid::RefreshGrid);
 }
 
-void UInventoryGrid::CreateLine(class UInventoryComponent *InventoryComp, float Tilesize)
+void UInventoryGrid::InitGrid()
 {
-	for (int i = 0; i <= InventoryComp->GetColumns(); i++)
-	{
-		LINES.Add(FInventoryLine(FVector2D(i * Tilesize, 0), FVector2D(i * Tilesize, InventoryComp->GetRows() * Tilesize)));
-	}
-	for (int i = 0; i <= InventoryComp->GetRows(); i++)
-	{
-		LINES.Add(FInventoryLine(FVector2D(0, i * Tilesize), FVector2D(InventoryComp->GetColumns() * Tilesize, i * Tilesize)));
-	}
-}
-
-void UInventoryGrid::RefreshGridInventory()
-{
-	GridCanvas->ClearChildren();
-	auto *Player = Cast<APlayerCharacter>(GetOwningPlayerPawn());
+	Player = Cast<APlayerCharacter>(GetOwningPlayerPawn());
 	if (Player)
 	{
-		ReferenceInventory = Player->GetItemInventory();
-		if (ReferenceInventory)
+		InventoryComponent = Player->GetItemInventory();
+		InventorySize = InventoryComponent->GetItemInventorySize();
+		InventoryWidth = InventoryComponent->GetInventoryWidth();
+		WidgetInventory = InventoryComponent->GetItemInventory();
+	}
+}
+
+void UInventoryGrid::RefreshGrid()
+{
+	InitGrid();
+	SetUpEmptyGrid();
+	GridInventory.Empty();
+	TArray<FVector2D> AllKeys;
+	WidgetInventory.GetKeys(AllKeys);
+	for (int32 i = 0; i < AllKeys.Num(); i++)
+	{
+		if (InventoryItem && WidgetInventory[AllKeys[i]])
 		{
-			TMap<class APickUpItem *, FVector2D> ItemMaps = ReferenceInventory->GetAllItems();
-			for (const auto &Item : ItemMaps)
-			{
-				ItemWidget = CreateWidget<UInventoryItem>(GetOwningPlayer(), InventoryItem);
-				//ItemWidget->OnRemoveItem.AddDynamic(this, &UInventoryGird::RemoveItemInWidget);
-				ItemWidget->InitializeInventoryItem(Item.Key);
-				UPanelSlot *PanelSlot = GridCanvas->AddChild(ItemWidget);
-				UCanvasPanelSlot *CanvasPanelSlot = Cast<UCanvasPanelSlot>(PanelSlot);
-				CanvasPanelSlot->SetAutoSize(true);
-				CanvasPanelSlot->SetPosition(FVector2D(Item.Value.X * GridTileSize, Item.Value.Y * GridTileSize));
-			}
+			ItemWidget = CreateWidget<UInventoryItem>(GetOwningPlayer(), InventoryItem);
+			ItemWidget->InitializeInventoryItem(WidgetInventory[AllKeys[i]]);
+			auto GridPanelSlot = InventoryGrid->AddChildToGrid(ItemWidget, AllKeys[i].Y, AllKeys[i].X);
+			GridPanelSlot->SetLayer(1);
+			GridPanelSlot->SetColumnSpan(WidgetInventory[AllKeys[i]]->GetMaxSize(WidgetInventory[AllKeys[i]]->GetItemRotation(), true).X);
+			GridPanelSlot->SetRowSpan(WidgetInventory[AllKeys[i]]->GetMaxSize(WidgetInventory[AllKeys[i]]->GetItemRotation(), true).Y);
 		}
 	}
 }
 
-void UInventoryGrid::RemoveItemInWidget(class APickUpItem *PickUpItem)
+void UInventoryGrid::SetUpEmptyGrid()
 {
-	ReferenceInventory->RemoveItem(PickUpItem);
-	RefreshGridInventory();
-}
-
-int32 UInventoryGrid::NativePaint(const FPaintArgs &Args, const FGeometry &AllottedGeometry, const FSlateRect &MyCullingRect, FSlateWindowElementList &OutDrawElements, int32 LayerId, const FWidgetStyle &InWidgetStyle, bool bParentEnabled) const
-{
-	FPaintContext Context(AllottedGeometry, MyCullingRect, OutDrawElements, LayerId, InWidgetStyle, bParentEnabled);
-
-	//FVector2D TopLeft = GridBorder->GetCachedGeometry().LocalToAbsolute(FVector2D::ZeroVector);
-	FVector2D TopLeft = FVector2D::ZeroVector;
-	for (int i = 0; i < LINES.Num(); i++)
+	GridState.Empty();
+	GridSlot.Empty();
+	InventoryGrid->ClearChildren();
+	int32 Column = 0;
+	int32 Row = 0;
+	for (int32 i = 0; i < InventorySize; i++)
 	{
-		UWidgetBlueprintLibrary::DrawLine(Context, TopLeft + LINES[i].Start, TopLeft + LINES[i].End, FLinearColor::White, true, 1.0f);
+		GridState.Add(FVector2D(Column, Row), false);
+		if (InventorySlot)
+		{
+			SlotWidget = CreateWidget<UInventorySlot>(GetOwningPlayer(), InventorySlot);
+			GridSlot.Add(SlotWidget);
+			InventoryGrid->AddChildToGrid(SlotWidget, Row, Column);
+			if (Column == InventoryWidth - 1)
+			{
+				Column = 0; 
+				Row++;
+			}
+			else
+			{
+				Column++;
+			}
+		}
 	}
-	return Super::NativePaint(Args, AllottedGeometry, MyCullingRect, OutDrawElements, LayerId, InWidgetStyle,
-		bParentEnabled);
 }
-
-
-
