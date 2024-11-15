5435b8054fcb5bb3858092c171826dbb8de8dc4b
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-11-15 22:05:48 +0900

    NPC 캐릭터 추가

---
+++ Source/UE5_TimeTravleHunter/Private/Character/Enemy/EnemyCharacter.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+// GameFramework
+#include "Character/Enemy/EnemyCharacter.h"
+#include "Character/Player/PlayerCharacter.h"
+
+// Engine
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+AEnemyCharacter::AEnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	AssasinationCollision = CreateDefaultSubobject<USphereComponent>(TEXT("AssasinationColl"));
+	AssasinationCollision->SetupAttachment(RootComponent);
+}
+
+void AEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	TargetCharacter = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+
+	AssasinationCollision->OnComponentBeginOverlap.AddDynamic(this, &AEnemyCharacter::OnOverlapBegin);
+	AssasinationCollision->OnComponentEndOverlap.AddDynamic(this, &AEnemyCharacter::OnOverlapEnd);
+}
+
+void AEnemyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AEnemyCharacter::OnOverlapBegin(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+{
+	if (auto Target = Cast<APlayerCharacter>(OtherActor))
+	{
+		bIsAssasinationable = true;
+	}
+}
+
+void AEnemyCharacter::OnOverlapEnd(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex)
+{
+	if (auto Target = Cast<APlayerCharacter>(OtherActor))
+	{
+		bIsAssasinationable = true;
+	}
+}
+
+void AEnemyCharacter::StealthAssain(FVector &Location, FRotator &Rotation)
+{
+
+}
