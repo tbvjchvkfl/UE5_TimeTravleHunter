ce57544a01f45e38f4cd6d875354eac18406802c
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-11-26 21:43:14 +0900

    AdvancedLocomotion 작업 진행중

--- Source/UE5_TimeTravleHunter/Public/AnimInstance/PlayerAnimInstance.h
+++ Source/UE5_TimeTravleHunter/Public/AnimInstance/PlayerAnimInstance.h
@@ -4,11 +4,24 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "Animation/CachedAnimData.h"
 #include "PlayerAnimInstance.generated.h"
 
 
 class APlayerCharacter;
 class APlayerCharacterController;
+class UAnimMontage;
+struct FCachedAnimStateData;
+
+UENUM()
+enum class ECharacterMovementState : uint8
+{
+	IDLE UMETA(Displayname = "Idle"),
+	WALK UMETA(Displayname = "Walk"),
+	JOG UMETA(Displayname = "Jog"),
+	SPRINT UMETA(Displayname = "Sprint"),
+	JUMP UMETA(Displayname = "Jump")
+};
 
 UCLASS()
 class UE5_TIMETRAVLEHUNTER_API UPlayerAnimInstance : public UAnimInstance
@@ -18,13 +31,30 @@ public:
 	//======================================================
 	//=					- Variables -					   =
 	//======================================================
-	
+	UPROPERTY(EditAnywhere, Category = "Animation | Locomotion")
+	UAnimMontage *CrouchVaulting_Anim;
+
+	UPROPERTY(EditAnywhere, Category = "Animation | Locomotion")
+	UAnimMontage *NormalVaulting_Anim;
+
+	UPROPERTY(EditAnywhere, Category = "Animation | Locomotion")
+	UAnimMontage *Hurdling_Anim;
+
+	UPROPERTY(EditAnywhere, Category = "Animation | Action")
+	UAnimMontage *Assasination_Anim;
+
+	// MontageDelegate
+	FOnMontageEnded VaultMontageDelegate;
 
 	//======================================================
 	//=					- Functionary -					   =
 	//======================================================
-	virtual void NativeInitializeAnimation() override;
-	virtual void NativeUpdateAnimation(float DeltaSeconds)override;
+	void PlayCrouchVaulting();
+	void PlayNormalVaulting();
+	void PlayHurdling();
+	void PlayAssasination();
+
+	float SafeDivide(float Numerator, float Denominator, float DefaultValue = 0)const;
 
 private:
 	//======================================================
@@ -37,6 +67,12 @@ private:
 	APlayerCharacterController *OwnerController;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|LocoMotion", meta = (AllowPrivateAccess = "true"))
+	ECharacterMovementState CharacterMovementState;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|LocoMotion", meta = (AllowPrivateAccess = "true"))
+	FVector CharacterVelocity;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|LocoMotion", meta = (AllowPrivateAccess = "true"))
 	float MovementYawDelta;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|LocoMotion", meta = (AllowPrivateAccess = "true"))
@@ -44,6 +80,12 @@ private:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|LocoMotion", meta = (AllowPrivateAccess = "true"))
 	float MovementSpeed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|LocoMotion", meta = (AllowPrivateAccess = "true"))
+	float MaxSpeed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|LocoMotion", meta = (AllowPrivateAccess = "true"))
+	FVector LastInputVector;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|LocoMotion", meta = (AllowPrivateAccess = "true"))
 	bool bIsAccelation;
@@ -64,12 +106,48 @@ private:
 	bool bIsAimming;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|Action", meta = (AllowPrivateAccess = "true"))
-	bool bIsPistol;
+	bool bIsParkour;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|Action", meta = (AllowPrivateAccess = "true"))
-	bool bIsRifle;
+	float PlayRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|Action", meta = (AllowPrivateAccess = "true"))
+	FRotator StartRotation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|Action", meta = (AllowPrivateAccess = "true"))
+	FRotator PrimaryTargetRotation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|Action", meta = (AllowPrivateAccess = "true"))
+	FRotator SecondaryTargetRotation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|Action", meta = (AllowPrivateAccess = "true"))
+	float WalkStartRotation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterState|Action", meta = (AllowPrivateAccess = "true"))
+	float JogStartRotation;
+
+	UPROPERTY()
+	FCachedAnimStateData WalkStateData;
+
+	UPROPERTY()
+	FCachedAnimStateData JogStateData;
+
+	UPROPERTY()
+	FCachedAnimStateData LocomotionStateData;
+
+	bool bIsPlayWalkStart;
+	bool bIsPlayJogStart;
 	//======================================================
 	//=					- Functionary -					   =
 	//======================================================
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds)override;
+	void SetMovementData();
+	bool IsMovementWithInThresholds(float MinCurrentSpeed, float MinMaxSpeed, float MinInputAccelation) const;
+	void DetermineLocomotionState();
+	void TrackLocomotionState(ECharacterMovementState LocomotionState);
 
+	void UpdateLocomotionValue();
+	void UpdateOnWalkEntry();
+	void UpdateOnJogEntry();
 };
