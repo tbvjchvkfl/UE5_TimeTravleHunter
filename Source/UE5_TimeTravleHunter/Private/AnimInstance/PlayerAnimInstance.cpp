a30d907c805c309050fe3d4f36961da3465501c5
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-12-18 22:10:40 +0900

    Locomotion 버그 수정 진행중

--- Source/UE5_TimeTravleHunter/Private/AnimInstance/PlayerAnimInstance.cpp
+++ Source/UE5_TimeTravleHunter/Private/AnimInstance/PlayerAnimInstance.cpp
@@ -53,10 +53,10 @@ void UPlayerAnimInstance::InitAnimationInstance()
 	MovementSpeed = 0.0f;
 	MovementStartAngle = 0.0f;
 	CharacterVelocity = FVector::ZeroVector;
-	CharacterMovementState = ECharacterMovementState::IDLE;
-	CharacterMovementDirection = ECharacterMovementDirection::FOWARD;
 	CanWalkStartSkip = false;
 	CanJogStartSkip = false;
+
+	
 }
 
 void UPlayerAnimInstance::SetMovementData()
@@ -72,16 +72,17 @@ void UPlayerAnimInstance::SetMovementData()
 	bIsSprint = OwnerController->bIsSprint;
 	bIsParkour = OwnerController->bIsParkour;
 
-	FRotator BaseAimRotation = OwnerCharacter->GetBaseAimRotation();
-	FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(OwnerCharacter->GetVelocity());
-	MovementYawDelta = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, BaseAimRotation).Yaw;
+	MovementYawDelta = UKismetMathLibrary::NormalizedDeltaRotator(UKismetMathLibrary::MakeRotFromX(OwnerCharacter->GetVelocity()), OwnerCharacter->GetBaseAimRotation()).Yaw;
 
-	SetMaxAccelAndPlayRate();
+	SetMaxSpeedAndPlayRate();
 	SetRotationRate(0.0f, 500.0f);
-	DetermineMovementDirection();
+	//DetermineMoveStartAnim();
+	DetermineMoveEndAnim();
+
+	GEngine->AddOnScreenDebugMessage(1, 3, FColor::Green, FString::Printf(TEXT("GetActorRotation Yaw : %f"), OwnerCharacter->GetActorRotation().Yaw));
 }
 
-void UPlayerAnimInstance::SetMaxAccelAndPlayRate()
+void UPlayerAnimInstance::SetMaxSpeedAndPlayRate()
 {
 	float ClampValue = FMath::Clamp(GetCurveValue(FName("MoveData_Speed")), 50.0f, 1000.0f);
 	PlayRate = UKismetMathLibrary::SafeDivide(MovementSpeed, ClampValue);
@@ -109,40 +110,67 @@ bool UPlayerAnimInstance::SetMovementDirection(float MinValue, float MaxValue, b
 			return true;
 		}
 		Direction = 0.0f;
-		return false;
 	}
 	return false;
 }
 
-void UPlayerAnimInstance::DetermineMovementDirection()
+void UPlayerAnimInstance::DetermineMoveStartAnim()
 {
-	if (MovementSpeed <= 5.0f)
+	if (MovementSpeed < 5.0f)
 	{
 		if (SetMovementDirection(-180.0f, -135.0f, true, false, MovementStartAngle))
 		{
-			CharacterMovementDirection = ECharacterMovementDirection::TURNLEFT_180;
+			DesiredStartMoveAnim(MovementAnimStruct.WalkStartLeft_180, MovementAnimStruct.JogStartLeft_180);
+			GEngine->AddOnScreenDebugMessage(3, 3, FColor::Green, FString::Printf(TEXT("MovementStartAngle : %f"), MovementStartAngle));
 			return;
 		}
 		else if (SetMovementDirection(-135.0f, -45.0f, true, false, MovementStartAngle))
 		{
-			CharacterMovementDirection = ECharacterMovementDirection::TURNLEFT_90;
+			DesiredStartMoveAnim(MovementAnimStruct.WalkStartLeft_90, MovementAnimStruct.JogStartLeft_90);
+			GEngine->AddOnScreenDebugMessage(3, 3, FColor::Green, FString::Printf(TEXT("MovementStartAngle : %f"), MovementStartAngle));
 			return;
 		}
 		else if (SetMovementDirection(45.0f, 135.0f, false, true, MovementStartAngle))
 		{
-			CharacterMovementDirection = ECharacterMovementDirection::TURNRIGHT_90;
+			DesiredStartMoveAnim(MovementAnimStruct.WalkStartRight_90, MovementAnimStruct.JogStartRight_90);
+			GEngine->AddOnScreenDebugMessage(3, 3, FColor::Green, FString::Printf(TEXT("MovementStartAngle : %f"), MovementStartAngle));
 			return;
 		}
 		else if (SetMovementDirection(135.0f, 180.0f, false, true, MovementStartAngle))
 		{
-			CharacterMovementDirection = ECharacterMovementDirection::TURNRIGHT_180;
+			DesiredStartMoveAnim(MovementAnimStruct.WalkStartRight_180, MovementAnimStruct.JogStartRight_180); 
+			GEngine->AddOnScreenDebugMessage(3, 3, FColor::Green, FString::Printf(TEXT("MovementStartAngle : %f"), MovementStartAngle));
 			return;
 		}
 		else
 		{
-			CharacterMovementDirection = ECharacterMovementDirection::FOWARD;
+			DesiredStartMoveAnim(MovementAnimStruct.WalkStartForward, MovementAnimStruct.JogStartForward);
 			return;
 		}
+	}
+}
+
+void UPlayerAnimInstance::DetermineMoveEndAnim()
+{
+	if (bIsWalk && !bIsJog)
+	{
+		DesiredMoveEndAnim = MovementAnimStruct.WalkEnd;
+	}
+	if (bIsJog && !bIsWalk)
+	{
+		DesiredMoveEndAnim = MovementAnimStruct.JogEnd;
+	}
+}
+
+void UPlayerAnimInstance::DesiredStartMoveAnim(UAnimSequenceBase *DesiredWalkAnim, UAnimSequenceBase *DesiredJogAnim)
+{
+	if (bIsWalk && !bIsJog)
+	{
+		DesiredMoveStartAnim = DesiredWalkAnim;
+	}
+	if (!bIsWalk && bIsJog)
+	{
+		DesiredMoveStartAnim = DesiredJogAnim;
 	}
 }
 
@@ -150,81 +178,82 @@ void UPlayerAnimInstance::FootIK(float DeltaSecond)
 {
 	if (!bIsInAir)
 	{
-		TTuple<bool, float> Foot_L = CapsuleDistance("ik_foot_l");
-		TTuple<bool, float> Foot_R = CapsuleDistance("ik_foot_r");
+		float Foot_L = CapsuleDistance("ik_foot_l");
+		float Foot_R = CapsuleDistance("ik_foot_r");
 
-		if (Foot_L.Get<0>() || Foot_R.Get<0>())
+		if (Foot_L != 0.0f || Foot_R != 0.0f)
 		{
-			const float SelectFloat = UKismetMathLibrary::SelectFloat(Foot_L.Get<1>(), Foot_R.Get<1>(), Foot_L.Get<1>() >= Foot_R.Get<1>());
+			const float PelvisTargetDisitance = Foot_L >= Foot_R ? Foot_L : Foot_R;
 
-			Displacement = FMath::FInterpTo(Displacement, (SelectFloat - 98.0f) * -1.0f, DeltaSecond, CurrentInterpSpeed);
+			Pelvis = FMath::FInterpTo(Pelvis, (PelvisTargetDisitance - 100.0f) * -1.0f, DeltaSecond, CurrentInterpSpeed);
 
-			TTuple<bool, float, FVector> FootTrace_L = FootTrace("ik_foot_l");
-			TTuple<bool, float, FVector> FootTrace_R = FootTrace("ik_foot_r");
+			TTuple<float, FVector> FootTrace_L = FootTrace("ik_foot_l");
+			TTuple<float, FVector> FootTrace_R = FootTrace("ik_foot_r");
 
-			const float Distance_L = FootTrace_L.Get<1>();
-			const FVector FootLVector = FootTrace_L.Get<2>();
+			const float Distance_L = FootTrace_L.Get<0>();
+			const FVector FootLVector = FootTrace_L.Get<1>();
 			const FRotator MakeLRot(UKismetMathLibrary::DegAtan2(FootLVector.X, FootLVector.Z) * -1.0f, 0.0, UKismetMathLibrary::DegAtan2(FootLVector.Y, FootLVector.Z));
 
-			L_FootRotation = FMath::RInterpTo(L_FootRotation, MakeLRot, DeltaSecond, CurrentInterpSpeed);
 			L_FootIK = FMath::FInterpTo(L_FootIK, (Distance_L - 110.0f) / -45.0f, DeltaSecond, CurrentInterpSpeed);
+			L_FootRotation = FMath::RInterpTo(L_FootRotation, MakeLRot, DeltaSecond, CurrentInterpSpeed);
 
-			const float Distance_R = FootTrace_R.Get<1>();
-			const FVector FootRVector = FootTrace_R.Get<2>();
+			const float Distance_R = FootTrace_R.Get<0>();
+			const FVector FootRVector = FootTrace_R.Get<1>();
 			const FRotator MakeRRot(UKismetMathLibrary::DegAtan2(FootRVector.X, FootRVector.Z) * -1.0f, 0.0f, UKismetMathLibrary::DegAtan2(FootRVector.Y, FootRVector.Z));
 
-			R_FootRotation = FMath::RInterpTo(R_FootRotation, MakeRRot, DeltaSecond, CurrentInterpSpeed);
 			R_FootIK = FMath::FInterpTo(R_FootIK, (Distance_R - 110.0f) / -45.0f, DeltaSecond, CurrentInterpSpeed);
+			R_FootRotation = FMath::RInterpTo(R_FootRotation, MakeRRot, DeltaSecond, CurrentInterpSpeed);
 		}
 	}
 	else
 	{
-		L_FootRotation = FMath::RInterpTo(L_FootRotation, FRotator::ZeroRotator, DeltaSecond, CurrentInterpSpeed);
 		L_FootIK = FMath::FInterpTo(L_FootIK, 0.0f, DeltaSecond, CurrentInterpSpeed);
+		L_FootRotation = FMath::RInterpTo(L_FootRotation, FRotator::ZeroRotator, DeltaSecond, CurrentInterpSpeed);
 
-		R_FootRotation = FMath::RInterpTo(R_FootRotation, FRotator::ZeroRotator, DeltaSecond, CurrentInterpSpeed);
 		R_FootIK = FMath::FInterpTo(R_FootIK, 0.0f, DeltaSecond, CurrentInterpSpeed);
+		R_FootRotation = FMath::RInterpTo(R_FootRotation, FRotator::ZeroRotator, DeltaSecond, CurrentInterpSpeed);
 	}
 }
 
-TTuple<bool, float> UPlayerAnimInstance::CapsuleDistance(FName SocketName)
+float UPlayerAnimInstance::CapsuleDistance(FName SocketName)
 {
-	FVector WorldLocation{ OwnerCharacter->GetMesh()->GetComponentLocation() };
-	FVector BreakVector{ WorldLocation + FVector(0.0f, 0.0f, 98.0f) };
+	FVector MeshLocation = OwnerCharacter->GetMesh()->GetComponentLocation();
+	FVector PelvisLocation = MeshLocation + FVector(0.0f, 0.0f, 98.0f);
 
-	FVector SocketLocation{ OwnerCharacter->GetMesh()->GetSocketLocation(SocketName) };
-	FVector StartPos{ SocketLocation.X, SocketLocation.Y, BreakVector.Z };
-	FVector EndPos{ StartPos - FVector(0.0f, 0.0f, 151.0f) };
-	FHitResult CapsuleHit;
-	FCollisionQueryParams CapColParams;
-	CapColParams.AddIgnoredActor(OwnerCharacter);
+	FVector SocketLocation = OwnerCharacter->GetMesh()->GetSocketLocation(SocketName);
+	FVector StartPos = FVector(SocketLocation.X, SocketLocation.Y, PelvisLocation.Z);
+	FVector EndPos = FVector(StartPos - FVector(0.0f, 0.0f, 151.0f));
+
+	FHitResult PelvisHit;
+	FCollisionQueryParams PelvisCollisionParams;
+	PelvisCollisionParams.AddIgnoredActor(OwnerCharacter);
 
 
-	if (GetWorld()->LineTraceSingleByChannel(CapsuleHit, StartPos, EndPos, ECollisionChannel::ECC_Visibility, CapColParams))
+	if (GetWorld()->LineTraceSingleByChannel(PelvisHit, StartPos, EndPos, ECollisionChannel::ECC_Visibility, PelvisCollisionParams))
 	{
-		return MakeTuple(true, CapsuleHit.Distance);
+		return PelvisHit.Distance;
 	}
 
-	return MakeTuple(false, CapsuleHit.Distance);
+	return 0.0f;
 }
 
-TTuple<bool, float, FVector> UPlayerAnimInstance::FootTrace(FName SocketName)
+TTuple<float, FVector> UPlayerAnimInstance::FootTrace(FName SocketName)
 {
-	const FVector BoneLoc = OwnerCharacter->GetMesh()->GetSocketLocation(SocketName);
-	const FVector RootLoc = OwnerCharacter->GetMesh()->GetSocketLocation("root");
+	const FVector FootLocation = OwnerCharacter->GetMesh()->GetSocketLocation(SocketName);
+	const FVector RootBoneLocation = OwnerCharacter->GetMesh()->GetSocketLocation("root");
 
 	FHitResult IKHit;
-	FVector StartPos{ BoneLoc.X, BoneLoc.Y, RootLoc.Z + 105.0f };
-	FVector EndPos{ StartPos - FVector(0.0f, 0.0f, 105.0f) };
+	FVector StartPos = FVector(FootLocation.X, FootLocation.Y, RootBoneLocation.Z + 105.0f);
+	FVector EndPos = StartPos - FVector(0.0f, 0.0f, 105.0f);
 	FCollisionQueryParams IKColParam;
 	IKColParam.AddIgnoredActor(OwnerCharacter);
 
 	if (GetWorld()->LineTraceSingleByChannel(IKHit, StartPos, EndPos, ECollisionChannel::ECC_Visibility, IKColParam))
 	{
-		return MakeTuple(true, IKHit.Distance, IKHit.Normal);
+		return MakeTuple(IKHit.Distance, IKHit.Normal);
 	}
 
-	return MakeTuple(false, 999.0f, FVector::ZeroVector);
+	return MakeTuple(999.0f, FVector::ZeroVector);
 }
 
 void UPlayerAnimInstance::PlayCrouchVaulting()
