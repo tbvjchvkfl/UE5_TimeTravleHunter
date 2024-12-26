e4cf288212527c463f70264c8dc063557f8998f3
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-12-26 21:36:34 +0900

    무기 추가

--- Source/UE5_TimeTravleHunter/Public/Character/Armor/WeaponBase.h
+++ Source/UE5_TimeTravleHunter/Public/Character/Armor/WeaponBase.h
@@ -1,1 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
 
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBase.generated.h"
+
+UCLASS()
+class UE5_TIMETRAVLEHUNTER_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	//======================================================
+	//=					- Variables -					   =
+	//======================================================
+	
+
+	//======================================================
+	//=					- Functionary -					   =
+	//======================================================
+	AWeaponBase();
+
+protected:
+	//======================================================
+	//=					- Variables -					   =
+	//======================================================
+	UPROPERTY(EditAnywhere, Category = "Weapon | Base Data")
+	UStaticMeshComponent *WeaponMesh;
+
+	//======================================================
+	//=					- Functionary -					   =
+	//======================================================
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+};
