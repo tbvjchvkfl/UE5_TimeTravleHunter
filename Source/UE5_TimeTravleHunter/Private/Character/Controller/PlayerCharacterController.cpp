fb53d17ec13bdb8fa1405fd76688834c61e436b6
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2024-12-26 21:33:28 +0900

    파일 위치 변경

---
+++ Source/UE5_TimeTravleHunter/Private/Character/Controller/PlayerCharacterController.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+// GameFramework
+#include "Character/Controller/PlayerCharacterController.h"
+#include "Character/Player/PlayerCharacter.h"
+#include "Object/PickUpItem.h"
+#include "UI/TTH_HUD.h"
+#include "UI/Inventory.h"
+
+// Engine
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Engine/LocalPlayer.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+#include "Kismet/GameplayStatics.h"
+
+APlayerCharacterController::APlayerCharacterController() : LookingRotationValue(1.0f)
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bIsJog = true;
+	bIsSprint = false;
+	bIsCrouch = false;
+	bIsAimming = false;
+	bIsParkour = false;
+	bIsKatanaState = false;
+	bIsSpearState = false;
+	bIsBowState = false;
+}
+
+void APlayerCharacterController::BeginPlay()
+{
+	Super::BeginPlay();
+	OwningHUD = Cast<ATTH_HUD>(GetHUD());
+	PlayerCharacter = Cast<APlayerCharacter>(GetPawn());
+}
+
+void APlayerCharacterController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (PlayerCharacter == nullptr)
+	{
+		PlayerCharacter = Cast<APlayerCharacter>(GetPawn());
+	}
+}
