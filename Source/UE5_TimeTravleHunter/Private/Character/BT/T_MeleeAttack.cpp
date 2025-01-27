c492f8126be15a61a1ef5e32144c0abe6a719f54
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-01-27 22:23:42 +0900

    BTTask노드 추가

---
+++ Source/UE5_TimeTravleHunter/Private/Character/BT/T_MeleeAttack.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+// GameFramework
+#include "Character/BT/T_MeleeAttack.h"
+#include "Character/Controller/EnemyCharacterController.h"
+#include "Character/Enemy/EnemyCharacter.h"
+#include "Character/AnimInstance/EnemyAnimInstance.h"
+
+// Engine
+
+
+UT_MeleeAttack::UT_MeleeAttack(FObjectInitializer const &ObjectInitializer)
+{
+	NodeName = "MeleeAttack";
+}
+
+EBTNodeResult::Type UT_MeleeAttack::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory)
+{
+	EBTNodeResult::Type Result = EBTNodeResult::Failed;
+	if (auto Controller = Cast<AEnemyCharacterController>(OwnerComp.GetAIOwner()))
+	{
+		if (auto ECharacter = Cast<AEnemyCharacter>(Controller->GetOwningCharacter()))
+		{
+			if (auto ECharacterAnimInstance = Cast<UEnemyAnimInstance>(ECharacter->GetMesh()->GetAnimInstance()))
+			{
+				Result = EBTNodeResult::InProgress;
+				if (ECharacterAnimInstance->IsAnyMontagePlaying())
+				{
+					Result = EBTNodeResult::Failed;
+				}
+				else
+				{
+					ECharacterAnimInstance->PlayMeleeAttack();
+					GetWorld()->GetTimerManager().SetTimer(DesiredNodeResultTimer, [&]()
+						{
+							GetWorld()->GetTimerManager().ClearTimer(DesiredNodeResultTimer);
+							FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+						}, ECharacterAnimInstance->MeleeAttack_Anim->GetPlayLength(), false);
+				}
+			}
+		}
+	}
+	return Result;
+}
--- Source/UE5_TimeTravleHunter/Private/Character/BT/T_MoveToRandomLocation.cpp
+++ Source/UE5_TimeTravleHunter/Private/Character/BT/T_MoveToRandomLocation.cpp
@@ -1,1 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
 
+
+// GameFramework
+#include "Character/BT/T_MoveToRandomLocation.h"
+#include "Character/Controller/EnemyCharacterController.h"
+#include "Character/Enemy/EnemyCharacter.h"
+
+// Engine
+#include "NavigationSystem.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+UT_MoveToRandomLocation::UT_MoveToRandomLocation(FObjectInitializer const &ObjectInitializer)
+{
+	NodeName = "MoveToRandomLocation";
+}
+
+EBTNodeResult::Type UT_MoveToRandomLocation::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory)
+{
+	if (const auto Controller = Cast<AEnemyCharacterController>(OwnerComp.GetAIOwner()))
+	{
+		if (const auto PlayerCharacter = Cast<AEnemyCharacter>(Controller->GetOwningCharacter()))
+		{
+			UNavigationSystemV1 *NavigationSystem = UNavigationSystemV1::GetCurrent(GetWorld());
+			//UNavigationSystemV1::GetNavigationSystem(GetWorld());
+			FNavLocation LocationInNaviMesh;
+			if (NavigationSystem->GetRandomPointInNavigableRadius(PlayerCharacter->GetActorLocation(), SearchRadius, LocationInNaviMesh))
+			{
+				OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), LocationInNaviMesh.Location);
+				return EBTNodeResult::Succeeded;
+			}
+		}
+	}
+	return EBTNodeResult::Failed;
+}
